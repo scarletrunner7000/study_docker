aa189a9842f167ce3527afbd01dd23f38bec7176
scarletrunner7000 <scarletrunner7000@gmail.com>
2017-03-30 03:01:18 +0900

    add customizing script

---
+++ customize.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+# https://docs.docker.com/engine/installation/linux/ubuntu
+sudo apt-get -y remove docker
+sudo apt-get -y update
+sudo apt-get -y install \
+    linux-image-extra-$(uname -r) \
+    linux-image-extra-virtual
+sudo apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get -y update
+sudo apt-get -y install docker-ce
+
